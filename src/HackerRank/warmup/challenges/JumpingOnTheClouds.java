6bf6fec2f979dbefb118b235b3bce66eabb4eec8
ABD EL RAOUF Ahmed <Ahmed.ABDELRAOUF@cegedim.com>
2020-04-05 15:25:10 +0200

    RepeatString HackerRank solved

--- src/HackerRank/warmup/challenges/JumpingOnTheClouds.java
+++ src/HackerRank/warmup/challenges/JumpingOnTheClouds.java
@@ -1,5 +1,28 @@
 package HackerRank.warmup.challenges;
 
 public class JumpingOnTheClouds {
+	static int solution(int[] c) {
+		int count = 0;
+		int i = 0;
+		while (i < c.length - 1) {
+			if (i + 2 >= c.length || c[i + 2] == 1) {
+				i++;
+				
+			} else {
+				i += 2;
+			}
+			count++;
+		}
+
+		return count;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(solution(new int[] { 0, 0, 0, 0, 1, 0 }));
+		System.out.println(solution(new int[] { 0, 0, 1, 0, 0, 1, 0 }));
+		System.out.println(solution(new int[] { 0, 0, 0, 0, 0, 0, 0 }));
+		System.out.println(solution(new int[] { 0, 1, 0, 0, 0, 0 }));
+		System.out.println(solution(new int[] { 0, 1, 1, 0, 1, 0 }));
+	}
 
 }
--- src/HackerRank/warmup/challenges/RepeatedString.java
+++ src/HackerRank/warmup/challenges/RepeatedString.java
@@ -1,5 +1,48 @@
+//Function Description
+//
+//Complete the repeatedString function in the editor below. It should return an integer representing the number of occurrences of a in the prefix of length  in the infinitely repeating string.
+//
+//repeatedString has the following parameter(s):
+//
+//s: a string to repeat
+//n: the number of characters to consider
+//Input Format
+//
+//The first line contains a single string, .
+//The second line contains an integer, .
 package HackerRank.warmup.challenges;
 
 public class RepeatedString {
+	static long repeatedString(String s, long n) {
 
+		long countForSubstring = 0;
+		long totalCount = 0;
+
+		// Determines how many a's are in a substring
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'a') {
+				countForSubstring++;
+			}
+		}
+
+		// Determines how many complete substrings we will analyze
+		long divisor = n / s.length();
+
+		totalCount += divisor * countForSubstring;
+
+		// Determines how many characters in we will analyze towards the end of our n
+		// range
+		long remainder = n % s.length();
+
+		for (int i = 0; i < remainder; i++) {
+			if (s.charAt(i) == 'a') {
+				totalCount++;
+			}
+		}
+		return totalCount;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(repeatedString("aba", 10));
+	}
 }
